5b56b93bef4cea4615d1907386e0a55bf482c2c1
jekidev <jeki_dev@proton.me>
2026-07-15 01:50:40 +0200

    Install global frontend network permission guard

--- artifacts/command-sim/src/main.tsx
+++ artifacts/command-sim/src/main.tsx
@@ -1,7 +1,10 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-import App from './App';
+import App from "./App";
+import { installNetworkPermissionGuard } from "./lib/network-permission-guard";
 
-import './index.css';
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(<App />);
+installNetworkPermissionGuard();
+
+createRoot(document.getElementById("root")!).render(<App />);
