54f7ca30f152bc01cf7999ea6a34b2e2f45c2ffe
jekidev <jeki_dev@proton.me>
2026-07-15 05:24:54 +0200

    Install authoritative advisor workspace request guard

--- artifacts/command-sim/src/main.tsx
+++ artifacts/command-sim/src/main.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import { installAdvisorRequestGuard } from "./lib/advisor-request-guard";
 import { installNetworkPermissionGuard } from "./lib/network-permission-guard";
 
 import "./index.css";
 
 installNetworkPermissionGuard();
+installAdvisorRequestGuard();
 
 createRoot(document.getElementById("root")!).render(<App />);
